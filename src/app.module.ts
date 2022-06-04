c90d7d0c8f3aa667695a822a4ff441954510dfc4
Luca Kiebel <github@luca-kiebel.de>
2022-06-04 22:06:58 +0200

    add mongoose to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,23 @@ import { KeycloakModule } from '@nibyou/keycloak';
 import { ChatModule } from './chat/chat.module';
 import { MessageModule } from './message/message.module';
 import { WssModule } from './wss/wss.module';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+
+const mongoOptions: MongooseModuleOptions = {
+  user: process.env.MONGO_USER || '',
+  pass: process.env.MONGO_PASS || '',
+  useNewUrlParser: true,
+  dbName: process.env.MONGO_DB || '',
+};
 
 @Module({
-  imports: [KeycloakModule, ChatModule, MessageModule, WssModule],
+  imports: [
+    KeycloakModule,
+    ChatModule,
+    MessageModule,
+    WssModule,
+    MongooseModule.forRoot(process.env.MONGO_URL, mongoOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
